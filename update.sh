b0f2714d1793ffde41b4129d3d8078a1d3ac306a
Nicolas Hurtubise <316k@legtux.org>
2014-09-21 17:07:10 -0400

    Creates the latest zip archive

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ cp -R includes output/includes
 sed s/DATE/$(date +%Y%j)/ templates/manifest.webapp |
 sed s/LATEST/$(ls output/*.html | wc -l)/ > output/manifest.webapp
 sed s/NUMBER/$(ls output/*.html | wc -l)/ templates/script.js > output/script.js
+cd output
+zip ../xkcd-archive.zip *
